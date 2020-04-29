72dba88c3d30ae911a3dbb8f294264bbaf0b4f32
FranMartiarena <franciscomartiarena@gmail.com>
2020-04-29 10:37:41 -0300

    some changes(class 6)

--- Classes/Class_6_Martiarena.sql
+++ Classes/Class_6_Martiarena.sql
@@ -8,17 +8,19 @@ select first_name, last_name from actor a1 where
 select first_name, last_name from actor a1 where last_name in (select last_name from actor a2
 		where a2.actor_id != a1.actor_id) order by last_name;
 
+--ex5
+
 select first_name, last_name, title  from film inner join film_actor on film.film_id = film_actor.film_id 
 					       inner join actor on film_actor.actor_id = actor.actor_id
 					       where film.title = 'BETRAYED REAR' or film.title = 'CATCH AMISTAD';
-
+--ex6
 select first_name, last_name, title  from film inner join film_actor on film.film_id = film_actor.film_id 
 					       inner join actor on film_actor.actor_id = actor.actor_id
 					       where film.title = 'BETRAYED REAR' and actor.actor_id not in (
 						select actor.actor_id from actor inner join film_actor on actor.actor_id = film_actor.actor_id 
 					       inner join film on film_actor.film_id = film.film_id
 					       where film.title = 'CATCH AMISTAD');
-					     
+--ex7					     
 select first_name, last_name, title  from film inner join film_actor on film.film_id = film_actor.film_id 
 					       inner join actor on film_actor.actor_id = actor.actor_id
 					       where film.title = 'BETRAYED REAR' and actor.actor_id  in (
