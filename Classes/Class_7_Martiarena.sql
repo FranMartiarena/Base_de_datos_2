4ff62353b9a674925edb54d721b6bb10d795baa6
FranMartiarena <40604256+FranMartiarena@users.noreply.github.com>
2020-05-06 10:22:53 -0300

    cambios

--- Classes/Class_7_Martiarena.sql
+++ Classes/Class_7_Martiarena.sql
@@ -13,10 +13,6 @@ select title, length, rating from film f1 where length <= all(select  length fro
  Show customer information, the address and the lowest amount, provide both solution using ALL
  and/or ANY and MIN.*/
 
-select first_name, address, amount from customer c1, address a1, payment p1 where 
-				c1.customer_id = p1.customer_id
-				and c1.address_id = a1.address_id 
-				and p1.amount <= all(select amount
-				 from customer c2, address a2, payment p2 where 
-					c2.customer_id = p2.customer_id
-					and c2.address_id = a2.address_id );
+select first_name, address, (select amount from payment where payment.customer_id = customer.customer_id 
+			and amount <= all(select amount from payment where customer.customer_id = payment.customer_id)limit 1) as amount
+			from customer, address where address.address_id = customer.address_id order by first_name;
