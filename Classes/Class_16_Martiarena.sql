3242024e6e0370fcbc2591ec1d74bc2bee4fe23f
FranMartiarena <40604256+FranMartiarena@users.noreply.github.com>
2020-08-25 22:57:50 -0300

    Class 16 update

--- Classes/Class_16_Martiarena.sql
+++ Classes/Class_16_Martiarena.sql
@@ -18,8 +18,8 @@ WHERE
 
 -- 3 Add a age column to the table employee where and it can only accept values from 16 up to 70 years old.
 
-
 -- alter table employees add column age int not null ; 
+
 alter table employees ADD CONSTRAINT check_age CHECK(age between 16 and 70);
 
 -- 4 Describe the referential integrity between tables film, actor and film_actor in sakila db.
@@ -30,19 +30,44 @@ alter table employees ADD CONSTRAINT check_age CHECK(age between 16 and 70);
  user that changed the row (assume multiple users, other than root, can connect to MySQL and change this table). */
 
 -- alter table employees add column lastUpdate datetime;
-DROP TRIGGER employee_last_update;
+-- alter table employees add column lastUpdateUser varchar(50);
+
+-- DROP TRIGGER employee_last_update;
 
 
 DELIMITER $$
 CREATE  TRIGGER employee_last_update 
-    after insert ON employees
+    before update ON employees
     FOR EACH ROW 
 BEGIN
-    INSERT INTO employees
-    SET 
-        lastUpdate = CURRENT_TIMESTAMP;
+    SET NEW.lastUpdate = NOW(),
+    NEW.lastUpdateUser = USER();
 END$$
 DELIMITER ;
 
+-- 6- Find all the triggers in sakila db related to loading film_text table. What do they do? Explain each of them using its source code for the explanation.
 
+-- SHOW TRIGGERS in sakila;
 
+-- After a film insert, filmtext will be inserted in the new film info
+/*BEGIN
+    INSERT INTO film_text (film_id, title, description)
+        VALUES (new.film_id, new.title, new.description);
+  END*/
+
+-- After a film update, if some film info is new, it will update film_text info about the film
+/*BEGIN
+    IF (old.title != new.title) OR (old.description != new.description) OR (old.film_id != new.film_id)
+    THEN
+        UPDATE film_text
+            SET title=new.title,
+                description=new.description,
+                film_id=new.film_id
+        WHERE film_id=old.film_id;
+    END IF;
+  END*/
+  
+  -- After a film delete, this trigger will delete the film from film_text
+/*BEGIN
+    DELETE FROM film_text WHERE film_id = old.film_id;
+  END*/
