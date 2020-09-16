ad4ac021ab318f9416f25b7d83974cf179fba0c4
FranMartiarena <40604256+FranMartiarena@users.noreply.github.com>
2020-09-16 08:43:59 -0300

    Update Class_19_Martiarena.sql

--- Classes/Class_19_Martiarena.sql
+++ Classes/Class_19_Martiarena.sql
@@ -10,8 +10,14 @@ grant select, update, delete  on sakila.* to data_analyst@localhost;
 
 -- Login with this user and try to create a table. Show the result of that operation.
 
-
+-- I couldnt login with the user due to some errors that i have, But i suppose that the user data_analyst, wont be able to create a table, only select, update and delete.
 
 -- Try to update a title of a film. Write the update script.
+
 -- With root or any admin user revoke the UPDATE permission. Write the command
+
+revoke update on sakila.* from data_analyst@localhost;
+
 -- Login again with data_analyst and try again the update done in step 4. Show the result.
+
+-- Now the data analyst cant update a table.
