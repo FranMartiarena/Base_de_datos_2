71b787bff620db082a95e5417618156e7f12c420
FranMartiarena <40604256+FranMartiarena@users.noreply.github.com>
2020-10-13 22:59:41 -0300

    cambios clase 21

--- Classes/Class_21_Martiarena/redisClient.py
+++ Classes/Class_21_Martiarena/redisClient.py
@@ -2,8 +2,33 @@ import redis
 
 r = redis.Redis(host='localhost', port=6379, db=0)
 
+#Strings
 r.set("key", "redis")
+print(r.get("key"))
+print(r.dump("key")) #serialized data
+print(f"The key Exists?={ r.exists('key')}")#Checks if the key exists, if so, it returns a 1, otherwise it will return a 0.
+r.delete("key")  #In python is "delete" instead of DEL, because "del" is a python keyword
+
+
+#Hashes
+#r.hmset("alumno:1","nombre","Francisco","age",1)
+#print(r.hget("alumno:1"))
+
+
+#lists
+iterations = 10
+for x in range(iterations):
+    r.lpush("counter", x)
+print(r.lrange("counter",0,-1))
+r.delete("counter")
 
 r.lpush("persona","fran")
 r.lpush("persona","martiarena")
 print(r.lrange("persona",0,-1))
+r.delete("persona")
+
+#Sets
+r.sadd("adds", "test1")
+r.sadd("adds", "test2")
+print(r.smembers("adds"))
+r.delete("adds")
